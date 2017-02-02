43ac7046f11137789e41740c1e11bbf619c3d951
Brendon <brennon.mckeever@gmail.com>
2017-02-02 12:09:15 -0600

    Database set up, need to work on HTML

--- Main.py
+++ Main.py
@@ -1,9 +1,20 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from band_db_manager import *
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///merch_sales.sqlite3'
 app.config['SECRET_KEY'] = "RockRules"
 
 db = SQLAlchemy(app)
+from band_db_manager import * # Needs to be after db, otherwise no tables are created.
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug = True)
--- band_db_manager.py
+++ band_db_manager.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from Main import db
-from datetime import datetime
+
 
 
 
