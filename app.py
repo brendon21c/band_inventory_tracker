f5901dbdf2b5c3f7d678c98cfd3e30337443abf2
Brendon <brennon.mckeever@gmail.com>
2017-02-04 20:29:51 -0600

    need to work on update function for concert data

--- app.py
+++ app.py
@@ -44,6 +44,27 @@ def new_item():
     return render_template('new_item.html', Items = Item.query.all())
 
 
+@app.route('/concert_details', methods = ['GET', 'POST'])
+def concert_details():
+
+    if request.method == 'POST':
+
+        # This should add a new item to the database but I'm gettting an error of database locked.
+        if request.form['button'] == "Add":
+
+            print('add button works')
+
+            new_item = Show(request.form['show'], request.form['item'], request.form['item_sold'])
+            #
+            db.session.add(new_item)
+            db.session.commit()
+            #return redirect(url_for('concert_details'))
+
+        else:
+            print('update button works')
+
+    return render_template('concert_details.html', concerts = Concert.query.all(), items = Item.query.all())
+
 
 
 
